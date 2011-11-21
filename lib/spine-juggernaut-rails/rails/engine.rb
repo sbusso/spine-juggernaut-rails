ca6939ca53b99ae29f5b2dfa28d9168450ac531f
Stephane Busso <stephane.busso@gmail.com>
2011-11-21 21:06:13 +0700

    add session_id to observed models

--- lib/spine-juggernaut-rails/rails/engine.rb
+++ lib/spine-juggernaut-rails/rails/engine.rb
@@ -4,12 +4,13 @@ module SpineJuggernautRails
 
     	observers = config.active_record.observers || []
     	config.active_record.observers = observers.is_a?(Array) ? observers << :juggernaut_observer : [observers, :juggernaut_observer]
-    	config.after_initialize do |app|
+    	config.to_prepare do |app|
     		SpineJuggernautRails.config do |juggernaut|
 	    		observee = juggernaut.observe.is_a?(Array) ? juggernaut.observe : [juggernaut.observe]
 					observee.each do |model_symbol|
 						model = model_symbol.to_s.split("_").collect(&:capitalize).join.constantize
-						#model.attr_accessor :session_id 
+						model.send :attr_accessor, :session_id
+    				# test .instance_variables include?(:session_id)
 					end
 
 					juggernaut.channel ||= '/observer' 
